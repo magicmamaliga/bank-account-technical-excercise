73b8f7af4a09e2cccfb2747572d3fd73bc891b9b
Mate Tornai <tornaimate95@gmail.com>
2025-09-18 09:57:37 +0200

    BT-backend 1 initialize and add root .gitignore

---
+++ balance-tracker/balance-tracker/src/test/java/com/mate/jpmc/balancetracker/BalanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mate.jpmc.balancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
